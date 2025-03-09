32334263c8c00841a3181e81c67ce27d73175e62
d3lvx <m4j3r@proton.me>
2025-03-09 12:33:56 -0400

    Added draw functionality in main, created the player instance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+from player import Player
 from constants import *
 
 
@@ -12,6 +13,7 @@ def main():
     pygame.init() # Initialize pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Assign the dimensions set in constants.py
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -20,10 +22,13 @@ def main():
                 return
             
         screen.fill((0,0,0)) # Fill the screen with a black background
+        player.draw(screen) # Drawing player on screen, has to be before display.flip()
         pygame.display.flip() # Update the full display Surface to the screen
 
 
         dt = clock.tick(60)/1000 # Setting to 60 FPS
+
+        
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,21 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+# Inherits from the CircleShape class in circleshape.py
+class Player(CircleShape):
+    def __init__(self,x,y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen,color=(255,255,255),points=self.triangle(), width=2)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
