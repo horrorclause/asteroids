6bafa0070b44ad67bca5604790d4606a26fc3db2
d3lvx <m4j3r@proton.me>
2025-03-09 10:37:34 -0400

    set to look for the event.type to quit, so screen can be closed

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initialize pygame
     pygame.init()
+
+    # Assign the dimensions set in constants.py
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0)) # Fill the screen with a black background
 
 
 
