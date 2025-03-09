b3651a830fbe0a36a1acbdc59c34bffa82ca45b4
d3lvx <m4j3r@proton.me>
2025-03-09 10:26:30 -0400

    initilize pygame, set screen dimensions and set the loop to fill the screen with black

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((0,0,0))
 
 
 
