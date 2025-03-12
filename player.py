219855b1b02eea500a7ce4061381f00b2ed3bf13
d3lvx <m4j3r@poton.me>
2025-03-12 17:25:12 -0400

    added timer

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen,color="red",points=self.triangle(), width=2)
@@ -28,6 +29,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED *dt
 
     def update(self,dt):
+
+        if self.timer > 0:
+            self.timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]: # What happens when lowercase "a" is pressed (left)
@@ -42,5 +47,11 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.timer > 0:
+            return None
+        
+        self.timer = PLAYER_SHOOT_COOLDOWN
+        
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        return shot
